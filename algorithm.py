a0d2348cb632f2fbab6f82c1a5536e53cc87ec9b
markoter <61563247+markoter@users.noreply.github.com>
2020-11-11 10:44:08 +0100

    start sudoku solver

---
+++ algorithm.py
@@ -0,0 +1,22 @@
+import array, random, math, time
+print("Sudoku solver v0.1")
+
+grid = [ [3, 0, 6, 5, 0, 8, 4, 0, 0], 
+         [5, 2, 0, 0, 0, 0, 0, 0, 0], 
+         [0, 8, 7, 0, 0, 0, 0, 3, 1], 
+         [0, 0, 3, 0, 1, 0, 0, 8, 0], 
+         [9, 0, 0, 8, 6, 3, 0, 0, 5], 
+         [0, 5, 0, 0, 9, 0, 6, 0, 0], 
+         [1, 3, 0, 0, 0, 0, 2, 5, 0], 
+         [0, 0, 0, 0, 0, 0, 0, 7, 4], 
+         [0, 0, 5, 2, 0, 6, 3, 0, 0] ]
+
+def is_empty(board):
+    for i in len(board):
+        for j in len(board[0]):
+            if board[i][j] == 0:
+                print(i + " " + j)
+                return(i,j)
+    return None
+
+is_empty(grid)
