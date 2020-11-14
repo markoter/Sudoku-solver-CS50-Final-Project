9dbe64bf80a13954ad3d1e03d39f330d1e1dcc51
markoter <61563247+markoter@users.noreply.github.com>
2020-11-14 20:56:39 +0100

    cleaning test.py functions

--- test.py
+++ test.py
@@ -7,14 +7,13 @@ grid = [ [3, 0, 6, 5, 0, 8, 4, 0, 0],
          [1, 3, 0, 0, 0, 0, 2, 5, 0], 
          [0, 0, 0, 0, 0, 0, 0, 7, 4], 
          [0, 0, 5, 2, 0, 6, 3, 0, 0] ]
-#create box for defined i and j
+#create box for defined i and j (there should be 9 boxes)
 def make_box(board, i, j):
+    # create empty 2d list
     box = [[None for _ in range(3)] for _ in range(3)]
     for m in range(3):
         for n in range(3):
-            modi = i % 3
-            modj = j % 3
-            box[m][n] = board[i + m - modi][j + n - modj]
+            box[m][n] = board[i + m - (i % 3)][j + n - (j % 3)]
     return box
 
 def search(number, board, i, j):
@@ -23,28 +22,23 @@ def search(number, board, i, j):
         if board[k][j] == number:
             # print(f"znalazłem {number} w ({k},{j})")
             return "znalazłem w kolumnie"
-
     # search horizontally
     for l in range(0, len(board)):
         if board[i][l] == number:
             # print(f"znalazłem {number} w ({i},{l})")
             return "znalazłem w linii"
-
     # search in box
     box = make_box(board, i, j)
+    # box is 2d list so we need to check every line one by one
     for row in box:
-        print(row)
         if number in row:
             return "znalazłem w rowie"
-    if number in box:
-        return "znalazłem w boxie"
-
     return "ni ma"
 
 
 # print wyniki
 szukany_numer = 2
-linia = 2
+linia = 1
 kolumna = 0
 
 wynik = search(szukany_numer, grid, linia, kolumna)
