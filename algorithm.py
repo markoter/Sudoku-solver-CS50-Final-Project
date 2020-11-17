324ddb3edf48e16b54e786431c0f938c28fa814d
markoter <61563247+markoter@users.noreply.github.com>
2020-11-17 17:52:25 +0100

    clean algorithm.py from unnecessary code

--- algorithm.py
+++ algorithm.py
@@ -1,10 +1,5 @@
 import array, random, math, time, sys
 print("***** Sudoku solver v0.1 *****")
-# recursion problem solver:
-# show limit
-print(sys.getrecursionlimit())
-# change limit
-# sys.setrecursionlimit(2500)
 
 # grid with only zeros
 zgrid = [ 
@@ -42,18 +37,6 @@ ogrid = [
     [9, 2, 0, 1, 7, 0, 3, 0, 5], 
     [8, 5, 1, 0, 4, 0, 0, 0, 0] ]
 
-# original tested grid sollution
-good_soll = [
-    [6, 8, 5, 2, 3, 1, 4, 7, 9],
-    [7, 3, 4, 8, 9, 5, 1, 6, 2],
-    [2, 1, 9, 4, 6, 7, 5, 3, 8],
-    [5, 6, 8, 7, 2, 4, 9, 1, 3],
-    [3, 4, 2, 5, 1, 9, 6, 8, 7],
-    [1, 9, 7, 3, 8, 6, 2, 5, 4],
-    [4, 7, 3, 6, 5, 2, 8, 9, 1],
-    [9, 2, 6, 1, 7, 8, 3, 4, 5],
-    [8, 5, 1, 9, 4, 3, 7, 2, 6] ]
-
 #create box for defined i and j (there should be 9 boxes)
 def make_box(board, i, j):
     # create empty 2d list
@@ -80,19 +63,6 @@ def search(number, board, i, j):
             return 1
     return 0
 
-"""
-# create new board to track empty spots
-def is_empty(board):
-    emptySpots = [[0 for i in range(9)] for j in range(9)]
-    for i in range(0, len(board)):
-        for j in range(0, len(board)):
-            if board[i][j] == 0:
-                # print(f"zero found in ({i}, {j})")
-                emptySpots[i][j] = 'o'
-            else:
-                emptySpots[i][j] = 'x'
-    return emptySpots
-"""
 # function to track empty spots positions in list of dicts
 def emptyList(board):
     emptySpot = []
@@ -141,8 +111,3 @@ print("Sudoku after changes:")
 newGrid = if_fits(grid, 0, emptySpots)
 for row in newGrid:
     print (row)
-
-if newGrid == good_soll:
-    print("sollution is good")
-else:
-    print("it doesn't match original sollution")
