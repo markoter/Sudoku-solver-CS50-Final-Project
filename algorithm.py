28808d985920e0b3ec65fd97dfbd3b5b7e52906b
markoter <61563247+markoter@users.noreply.github.com>
2020-11-17 17:50:54 +0100

    change recursion if_fits() to simple iteration

--- algorithm.py
+++ algorithm.py
@@ -1,15 +1,58 @@
-import array, random, math, time
+import array, random, math, time, sys
 print("***** Sudoku solver v0.1 *****")
+# recursion problem solver:
+# show limit
+print(sys.getrecursionlimit())
+# change limit
+# sys.setrecursionlimit(2500)
 
-grid = [ [3, 0, 6, 5, 0, 8, 4, 0, 0], 
-         [5, 2, 0, 0, 0, 0, 0, 0, 0], 
-         [0, 8, 7, 0, 0, 0, 0, 3, 1], 
-         [0, 0, 3, 0, 1, 0, 0, 8, 0], 
-         [9, 0, 0, 8, 6, 3, 0, 0, 5], 
-         [0, 5, 0, 0, 9, 0, 6, 0, 0], 
-         [1, 3, 0, 0, 0, 0, 2, 5, 0], 
-         [0, 0, 0, 0, 0, 0, 0, 7, 4], 
-         [0, 0, 5, 2, 0, 6, 3, 0, 0] ]
+# grid with only zeros
+zgrid = [ 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0] ]
+
+# hard grid that brakes the recursion
+grid = [ 
+    [0, 0, 0, 0, 0, 0, 0, 3, 0],
+    [0, 0, 0, 0, 9, 1, 0, 0, 0],
+    [8, 7, 0, 3, 0, 0, 0, 2, 0],
+    [0, 0, 4, 0, 0, 0, 0, 0, 2], 
+    [0, 9, 0, 0, 0, 0, 6, 1, 0],  
+    [3, 6, 0, 0, 0, 0, 0, 0, 0],  
+    [0, 3, 9, 0, 0, 0, 0, 6, 4], 
+    [0, 0, 0, 0, 1, 0, 0, 8, 0], 
+    [0, 2, 0, 0, 0, 0, 3, 0, 1] ]
+
+# original tested grid
+ogrid = [ 
+    [6, 0, 0, 0, 3, 1, 4, 0, 0], 
+    [0, 3, 0, 8, 0, 5, 0, 0, 2], 
+    [0, 0, 9, 0, 0, 0, 0, 0, 8], 
+    [5, 6, 0, 0, 2, 4, 0, 0, 0], 
+    [0, 4, 0, 0, 0, 0, 0, 8, 7], 
+    [1, 0, 7, 3, 8, 6, 2, 0, 0], 
+    [4, 0, 0, 6, 5, 0, 8, 9, 0], 
+    [9, 2, 0, 1, 7, 0, 3, 0, 5], 
+    [8, 5, 1, 0, 4, 0, 0, 0, 0] ]
+
+# original tested grid sollution
+good_soll = [
+    [6, 8, 5, 2, 3, 1, 4, 7, 9],
+    [7, 3, 4, 8, 9, 5, 1, 6, 2],
+    [2, 1, 9, 4, 6, 7, 5, 3, 8],
+    [5, 6, 8, 7, 2, 4, 9, 1, 3],
+    [3, 4, 2, 5, 1, 9, 6, 8, 7],
+    [1, 9, 7, 3, 8, 6, 2, 5, 4],
+    [4, 7, 3, 6, 5, 2, 8, 9, 1],
+    [9, 2, 6, 1, 7, 8, 3, 4, 5],
+    [8, 5, 1, 9, 4, 3, 7, 2, 6] ]
 
 #create box for defined i and j (there should be 9 boxes)
 def make_box(board, i, j):
@@ -80,15 +123,15 @@ def if_fits(board, index, emptySpots):
         board[i][j] = filler(ins, board, i, j)
         if board[i][j] == "$":
             index -= 1
-            return if_fits(board, index, emptySpots)
-        index += 1
+        else:
+            index += 1
     return board
 
 # debug printer
 emptySpot = emptyList(grid)
 print(f"empty spots to fill: {len(emptySpot)}")
 #for position in emptySpot:
- #  print(position)
+#  print(position)
 
 print("Original sudoku grid:")
 for row in grid:
@@ -98,3 +141,8 @@ print("Sudoku after changes:")
 newGrid = if_fits(grid, 0, emptySpots)
 for row in newGrid:
     print (row)
+
+if newGrid == good_soll:
+    print("sollution is good")
+else:
+    print("it doesn't match original sollution")
