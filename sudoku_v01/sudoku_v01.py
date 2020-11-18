04f971592c45ed70b226abe2e1a15cd58bbcc5e8
markoter <61563247+markoter@users.noreply.github.com>
2020-11-18 17:21:20 +0100

    finish sudoku v0.1

---
+++ sudoku_v01/sudoku_v01.py
@@ -0,0 +1,19 @@
+import array, random, math, time
+import solver as solver
+import grids as grids
+print("***** Sudoku solver v0.1 *****")
+
+grid = grids.grid
+emptySpots = solver.emptyList(grid)
+
+# debug printer
+print(f"Empty spots to fill: {len(emptySpots)}")
+
+print("Original sudoku grid:")
+for row in grid:
+    print(row)
+
+print("Sudoku after changes:")
+newGrid = solver.solve(grid, 0, emptySpots)
+for row in newGrid:
+    print (row)
--- test_filler.py
+++
@@ -1,8 +0,0 @@
-def filler(ins, board, i, j):
-    while ins < 10:
-        if search(ins, board, i, j) == 0:
-            return ins
-        else:
-            ins += 1
-            return filler(ins, board, i, j)
-    return "$"
