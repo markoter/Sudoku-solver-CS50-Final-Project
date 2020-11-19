89cb66c6f6ec62f87363ea03f834644f864dcd66
markoter <61563247+markoter@users.noreply.github.com>
2020-11-19 18:16:17 +0100

    repeat asking for row if len isn't 9

--- input.py
+++ input.py
@@ -11,12 +11,15 @@ def ask_by_elem():
 
 def ask_by_line():
     grid = []
-    for l in range (0,3):
-        line = (input(f"write line {l+1}: "))
-        if len(line) != 3:
-            print ("it myst be 9 numbers")
-            exit()
-        grid.append(list(line))
+    print("Write grid line by line: ")
+    for l in range (0,9):
+        while True:
+            line = (input(f"Write row {l+1}: "))
+            if len(line) == 9:
+                grid.append(list(line))
+                break
+            print("Row must be 9 numbers long!")
+        
     # print
     for row in grid:
         print(row)
