a048ece756cd16b0ea64ce28d3edf7f8cede1efa
markoter <61563247+markoter@users.noreply.github.com>
2020-11-19 17:55:02 +0100

    create another version of input

--- input.py
+++ input.py
@@ -1,9 +1,20 @@
-grid = []
-for i in range (0,3):
-    line = []
-    for j in range (0,3):
-        print("write element:")
-        line.append(input())
-    grid.append(line)
-for row in grid:
-    print(row)
+def ask_by_elem():
+    grid = []
+    for _ in range (0,3):
+        line = []
+        for _ in range (0,3):
+            print("write element:")
+            line.append(input())
+        grid.append(line)
+    for row in grid:
+        print(row)
+
+def ask_by_line():
+    grid = []
+    for _ in range (0,3):
+        line = (input("write line: "))
+        grid.append(line)
+    for row in grid:
+        print(row)
+
+ask_by_elem()
