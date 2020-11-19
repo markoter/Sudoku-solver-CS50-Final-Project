caf993f08e514e4a621b20e7b2b8dbf7416e0f83
markoter <61563247+markoter@users.noreply.github.com>
2020-11-19 19:07:26 +0100

    add option to create new grid to solve

--- sudoku_v02.py
+++ sudoku_v02.py
@@ -1,17 +1,27 @@
 import solver, grids
+from sys import argv, exit
 print("***** Sudoku solver v0.2 *****")
 
-grid = grids.grid
+if len(argv) != 2:
+    grid = grids.grid
+elif argv[1] == 'new':
+    grid = grids.create_grid()
+
 emptySpots = solver.emptyList(grid)
+
 
 # debug printer
 print(f"Empty spots to fill: {len(emptySpots)}")
 
 print("Original sudoku grid:")
+indx = 0
 for row in grid:
-    print(row)
+    indx +=1
+    print(f"#{indx}:{row}")
 
 print("Sudoku after changes:")
 newGrid = solver.solve(grid, 0, emptySpots)
+indx = 0
 for row in newGrid:
-    print (row)
+    indx +=1
+    print(f"#{indx}:{row}")
