d4ef32e282bb934cb2b5a1f205d66bccf096e1d0
markoter <61563247+markoter@users.noreply.github.com>
2020-12-04 10:41:12 +0100

    start with grid printer

--- sudoku_v03_gui.py
+++ sudoku_v03_gui.py
@@ -2,20 +2,37 @@ import pygame, sys
 
 fps = 10
 # set size of the grid
-size = windowWidth, windowHeight = 450, 450
+windowSize = int(90)
+windowMultiplier = int(5) # here modify the size of the window
+windowWidth = int(windowSize * windowMultiplier)
+windowHeight = int(windowSize * windowMultiplier)
+squareSize = int((windowSize * windowMultiplier) / 3)
+cellSize = int(squareSize / 3)
+
 # set colors
 white = (255,255,255)
+black = (0,0,0)
+lightGray = (200,200,200)
+
+def drawGrid(screen):
+    # minor lines
+    for x in range(0, windowWidth, cellSize):
+        pygame.draw.line(screen, lightGray, (x,0), (x,windowHeight))
+    
+    return None
 
 def main():
     # initialize the pygame
     pygame.init()
     fpsClock = pygame.time.Clock()
     # set screen
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode((windowWidth, windowHeight))
     # set name shown on window
     pygame.display.set_caption('Sudoku Solver v03 GUI')
     # set background
     screen.fill(white)
+
+    drawGrid(screen)
 
     while True:
         for event in pygame.event.get():
@@ -23,6 +40,7 @@ def main():
                 sys.exit()
         pygame.display.update()
         fpsClock.tick(fps)
+
 
 if __name__ == '__main__':
     main()
